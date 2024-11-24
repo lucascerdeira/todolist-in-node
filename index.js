b56cdb79025cf4b07aee50f4d55c1eafeff927e8
lcerdeira <lucas.cerdeira@segurossura.com.br>
2024-11-24 09:48:08 -0300

    alterando valor da porta

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = require('./src/routes/TaskRoutes');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(express.json()); 
 app.use('/api', router); 
